4f2106779165f4292966d461386307ecb829ff84
Angela Soler <asoler@c1r3p3.42sp.org.br>
2022-08-09 21:48:19 -0300

    wip RGB -> making structure

--- RGB.c
+++ RGB.c
@@ -1,10 +1,34 @@
+#include <stdio.h>
+
+void calcu_steps(char *init, char *end,int steps)
+{
+    int size;
+
+    if (end > init)
+    {
+        size = hex_to_int(end) - hex_to_int(init) / steps
+        put_steps(size, init, end);
+    }
+    else if (init > end)
+    {
+        size = - 1 * (hex_to_int(init) - hex_to_int(end)) / steps
+        put_steps_reverse(size, init, end);
+    }
+}
+
 int main(void)
 {
-    char    r_inicial;
-    char    r_final
+    char    *r_inicial;
+    char    *r_final;
     int     steps;
 
-    scanf("cor inicial: %s\ncor final: %s\nsteps: %d", r_inicial, r_final, steps);
-    print_steps(r_inicial, r_final, steps);
+    printf("cor inicial:");
+    scanf("%s", r_inicial);
+    printf("cor final:");
+    scanf("%s", r_final);
+    printf("steps:");
+    scanf("%d", &steps);
+    // calcu_steps(r_inicial, r_final, steps);
+    printf("%s, %s, %d", r_inicial, r_final, steps);
     return(0);
 }
