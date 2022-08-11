5dee0795e6d1303fa7106a9f86b75ab344b617f3
angela <manzanaembicicleta@gmail.com>
2022-08-11 09:41:46 -0300

    buble sort for linked list

--- mergeKLists.c
+++ mergeKLists.c
@@ -1,35 +1,114 @@
-struct ListaNode
+#include<stdio.h> 
+#include<stdlib.h> 
+
+
+struct ListNode
 {
-	int		n;
-	void	*next;
+	int				n;
+	struct ListNode	*next;
 };
 
-struct	ListaNode *mergeKListas(struct  ListNode **listas, int listasSize)
-{
-	struct ListNode **aux;
+void	insertAtTheBegin(struct ListNode **start_ref, int n);
+void	bubbleSort(struct ListNode *start);
+void	swap(struct ListNode *a, struct ListNode *b);
+void	printList(struct ListNode *start);
 
-	aux = listas;
-	while (*aux[i])
-	{
-		aux[i] = listas[i]->next;
-	}
-}
-
-int main(void)
+struct	ListNode *mergeKListas(struct ListNode **listas, int listasSize)
 {
-	typedef struct ListaNode	listas;
-	listas						**a;
-	listas						*b;
-	listas						*aux;
-	int							i;
+	struct ListNode	*aux;
+	struct ListNode	*result;
+	int		i;
 
 	i = 0;
-	b = mergeKListas(a, 5);
-	aux = b;
-	while (aux)
+	result = NULL;
+	while (i < listasSize)
 	{
-		printf("%d, ", b->n);
-		aux = aux->next;
+		aux = listas[i];
+		while (aux)
+		{
+			insertAtTheBegin(&result, aux->n);
+			aux = aux->next;
+		}
+		free(listas[i]);
+		i++;
 	}
-	return(0);
+	free(listas[i]);
+	free(listas);
+	bubbleSort(result);
+	return (result);
 }
+
+int	main(void)
+{
+	int	list0[] = {1, 3, 8};
+	int	list1[] = {1, 3, 6};
+	int	list2[] = {8, 9};
+	struct ListNode	**listas; 
+	struct ListNode	*result;
+	int	i;
+
+	listas = calloc(sizeof(struct ListNode *), 4);
+	 for (i = 0; i< 3; i++) 
+		insertAtTheBegin(&listas[0], list0[i]);
+	 for (i = 0; i< 3; i++) 
+		insertAtTheBegin(&listas[1], list1[i]);
+	 for (i = 0; i< 2; i++) 
+		insertAtTheBegin(&listas[2], list2[i]);
+	result = mergeKListas(listas, 3);
+	printf("output: ");
+	printList(result);
+}
+
+void	insertAtTheBegin(struct ListNode **start_ref, int n) 
+{ 
+	struct ListNode	*ptr1 = (struct ListNode*)malloc(sizeof(struct ListNode));
+	ptr1->n = n;
+	ptr1->next = *start_ref;
+	*start_ref = ptr1;
+} 
+
+void	printList(struct ListNode *start) 
+{ 
+	struct ListNode	*temp = start;
+
+	while (temp!=NULL)
+	{
+		printf("%d ", temp->n);
+		temp = temp->next;
+	}
+	printf("\n");
+}
+
+void	bubbleSort(struct ListNode *start) 
+{ 
+	int		swapped;
+	struct	ListNode *ptr1;
+	struct	ListNode *lptr;
+
+	if (start == NULL) 
+		return ;
+	swapped = 1;
+	lptr = NULL;
+	while (swapped)
+	{
+		swapped = 0;
+		ptr1 = start;
+		while (ptr1->next != lptr)
+		{ 
+			if (ptr1->n > ptr1->next->n)
+			{ 
+				swap(ptr1, ptr1->next);
+				swapped = 1;
+			} 
+			ptr1 = ptr1->next;
+		}
+		lptr = ptr1;
+	} 
+} 
+
+void	swap(struct ListNode *a, struct ListNode *b) 
+{
+	int temp = a->n;
+	a->n = b->n;
+	b->n = temp;
+} 
